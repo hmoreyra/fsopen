51bbd2bc268a1af4953c80b3d41c855c22c2a342
Hernán Moreyra <hernan_hello@hotmail.com>
2025-07-21 21:51:33 -0300

    exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,14 @@ import { useState } from 'react'
 
 const Button = ({text, handleClick}) => <button onClick={handleClick}> {text} </button>
 
-const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Statistics = ({good,neutral,bad}) => {
   const all = good+neutral+bad
@@ -10,14 +17,16 @@ const Statistics = ({good,neutral,bad}) => {
     <div>
       <h1>statistics</h1>
       {all != 0 ? 
-        <div>
-          <StatisticLine text="good"     value = {good} />
-          <StatisticLine text="neutral"  value = {neutral} />
-          <StatisticLine text="bad"      value = {bad} />
-          <StatisticLine text="all"      value = {all} />
-          <StatisticLine text="average"  value = {(good - bad) / all} />
-          <StatisticLine text="positive" value = {good * 100 / all} />
-        </div>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={(good - bad) / all} />
+            <StatisticLine text="positive" value={`${(good * 100 / all)} %`} />
+          </tbody>
+        </table>
         :
         <p>No feedback given</p>
       }
