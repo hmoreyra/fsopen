15f1044e68339ea37fc4025358677edfd954525a
Hernán Moreyra <hernan_hello@hotmail.com>
2025-07-22 21:04:49 -0300

    exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,18 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phoneNumber: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
 
   const handleSubmit = (e) =>{
     e.preventDefault()
-    if(persons.some(p => p.name === newName)){ // persons.map(p => p.name).includes(newName) works too, but it's more inefficient
-      alert(`${newName} is already added to phonebook`)
+    if(persons.some(p => p.name === newName || p.phoneNumber === phoneNumber)){
+      alert(`"Either the name '${newName}' or the phone number '${phoneNumber}' has already been added to the phonebook.`)
     }
     else{
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({name: newName, phoneNumber: phoneNumber}))
       setNewName('')
+      setPhoneNumber('')
     }
   }
 
@@ -25,11 +27,14 @@ const App = () => {
           name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
         </div>
         <div>
+          number: <input value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.phoneNumber}</p>)}
     </div>
   )
 }
